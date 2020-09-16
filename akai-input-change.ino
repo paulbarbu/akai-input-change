4db6e88b2d3bb83fd2abaafb3fb5066b95bd79e0
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2020-09-16 15:55:02 +0300

    some docs on the wiring

--- akai-input-change.ino
+++ akai-input-change.ino
@@ -1,3 +1,13 @@
+/** 
+ * Digitally "press" a physical button when the Akai SS034A-66TT home audio system starts in order to change the input from bluetooth to auxiliary
+ * 
+ * You have to identify which button changes the input for your speakers and connect its positive lead to pin D12.
+ * 
+ * Connections:
+ * 5V from the front panel (there is a USB port, use that) to +5V pin of the arduino nano
+ * GND of panel to GND of arduino nano
+ * positive input of "INPUT" switch of the front panel to pin D12  - this will pull the line to GND in order to simulate the button press
+ */
 
 int buttonPin = 12; // the pin where we connect the switch to
 
